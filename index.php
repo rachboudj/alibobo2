c0f9f94c1b0b8d78e4e0c99dc641e9668bb17aa9
Frédéric NOËL <contact@fredericnoel.com>
2022-06-27 15:11:24 +0200

    Appel avec require_once des fichiers includes

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+/* Utiliser include ou require
+* include renvoie un avertissement simple en cas d'erreur
+* require renvoie une erreur fatale et arrête l'exécution du script
+*/
+require_once './includes/header.php';
+require_once './includes/main.php';
+require_once './includes/footer.php';
