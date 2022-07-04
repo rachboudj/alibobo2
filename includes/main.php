b82866b66e26454b0c3fa04f26e99d96fff607e8
Frédéric NOËL <contact@fredericnoel.com>
2022-07-04 11:32:47 +0200

    Utilisation de l'opérateur null coalescent

--- includes/main.php
+++ includes/main.php
@@ -2,7 +2,21 @@
     <h1>Main</h1>
 <?php
 
-dump($_GET);
+// Syntaxe conventionnelle
+// if (isset($_GET['page'])) {
+//     $page = $_GET['page'];
+// }
+// else {
+//     $page = "accueil";
+// }
+
+// // Opérateur ternaire
+// $page = isset($_GET['page']) ? $_GET['page'] : "accueil";
+
+// Null coalescent operator
+$page = $_GET['page'] ?? "accueil";
+
+dump($page);
 
 ?>
 </main>
