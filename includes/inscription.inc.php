d2a5831903d1491c88a7239cddf63f71d9a64355
Frédéric NOËL <contact@fredericnoel.com>
2022-07-04 16:23:55 +0200

    Vérification des erreurs et écriture de la requête SQL d'insertion

--- includes/inscription.inc.php
+++ includes/inscription.inc.php
@@ -10,10 +10,50 @@ if (isset($_POST['frmInscription'])) {
     $email = isset($_POST['email']) ? htmlentities(trim($_POST['email'])) : "";
     $mdp1 = isset($_POST['mdp1']) ? htmlentities(trim($_POST['mdp1'])) :  "";
     $mdp2 = isset($_POST['mdp2']) ? htmlentities(trim($_POST['mdp2'])) :  "";
+
+    $erreurs = array();
+
+    if (mb_strlen($nom) === 0)
+        array_push($erreurs, "Veuillez saisir votre nom");
+
+    if (mb_strlen($prenom) === 0)
+        array_push($erreurs, "Veuillez saisir votre prénom");
+
+    if (mb_strlen($email) === 0)
+        array_push($erreurs, "Veuillez saisir une adresse mail");
+
+    elseif (!(filter_var($email, FILTER_VALIDATE_EMAIL)))
+        array_push($erreurs, "Veuillez saisir une adresse mail conforme");
+
+    if (mb_strlen($mdp1) === 0 || mb_strlen($mdp2) === 0) 
+        array_push($erreurs, "Veuillez saisis deux fois votre mot de passe");
+
+    elseif ($mdp1 !== $mdp2) 
+        array_push($erreurs, "Vos mots de passe ne sont pas identiques");
+
+    if (count($erreurs) > 0) {
+        $messageErreurs = "<ul>";
+
+        for ($i = 0 ; $i < count($erreurs) ; $i++) {
+            $messageErreurs .= "<li>";
+            $messageErreurs .= $erreurs[$i];
+            $messageErreurs .= "</li>";
+        }
+    
+        $messageErreurs .= "</ul>";
+    
+        echo $messageErreurs;
+    }
+
+    else {
+        $mdp1 = sha1($mdp1);
+        $requeteInscription = "INSERT INTO t_utilisateurs
+        (id_utilisateur, nom, prenom, email, mdp)
+        VALUES (NULL, '$nom', '$prenom', '$email', '$mdp1')
+        ";
+    }
 }
 
 else {
-    echo "Je viens du futur";
+    require_once 'frmInscription.php';
 }
-
-require_once 'frmInscription.php';
