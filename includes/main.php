3663da51a416c1093aba63700bbd7d91ee217cff
Frédéric NOËL <contact@fredericnoel.com>
2022-07-04 12:31:07 +0200

    Fichiers avec double extension

--- includes/main.php
+++ includes/main.php
@@ -2,21 +2,7 @@
     <h1>Main</h1>
 <?php
 
-// Syntaxe conventionnelle
-// if (isset($_GET['page'])) {
-//     $page = $_GET['page'];
-// }
-// else {
-//     $page = "accueil";
-// }
-
-// // Opérateur ternaire
-// $page = isset($_GET['page']) ? $_GET['page'] : "accueil";
-
-// Null coalescent operator
 $page = $_GET['page'] ?? "accueil";
-
-dump($page);
-
+autoInclude($page);
 ?>
 </main>
