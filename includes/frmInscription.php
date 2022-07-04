76abf75a260e2b3367da2336314e3f9761a5bf7d
Frédéric NOËL <contact@fredericnoel.com>
2022-07-04 14:31:26 +0200

    Formulaire inscription HTML

---
+++ includes/frmInscription.php
@@ -0,0 +1,26 @@
+<form action="index.php?page=inscription" method="post">
+    <div>
+        <label for="nom">Nom :</label>
+        <input type="text" id="nom" name="nom" />
+    </div>
+    <div>
+        <label for="prenom">Prénom :</label>
+        <input type="text" id="prenom" name="prenom" />
+    </div>
+    <div>
+        <label for="email">E-mail :</label>
+        <input type="text" id="email" name="email" />
+    </div>
+    <div>
+        <label for="mdp1">Mot de passe :</label>
+        <input type="password" id="mdp1" name="mdp1" />
+    </div>
+    <div>
+        <label for="mdp2">Vérification mot de passe :</label>
+        <input type="password" id="mdp2" name="mdp2" />
+    </div>
+    <div>
+        <input type="reset" value="Effacer" />
+        <input type="submit" value="Envoyer" />
+    </div>
+</form>
