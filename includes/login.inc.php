f125ba032e834f8ccfee821d3c47bc02c8a4ab6b
Akame <Akame@localhost>
2022-07-13 10:13:59 +0200

    formulaire avec email et mot de passe avec un bouton de validation de suppression de compte sans gestion de php

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -5,8 +5,12 @@
 	<h1>Login</h1>
 	<form action="" method="post" class="wrap2" novalidate id="desinscription">
         	<label for="email">E-mail :</label>
-        	<input type="email" name="email" id="email" value="<?php if(!empty($_POST>
-        	<span class="error"><?php if(!empty($errors['email'])) { echo $errors['em>
-        	<input type="submit" name="submitted" value="Ajouter un utilisateur">
+        	<input type="email" name="email" id="email" value="<?php if(!empty($_POST['email'])) { echo $_POST['email']; } ?>">
+        	<span class="error"><?php if(!empty($errors['email'])) { echo $errors['email']; } ?></span>
+		
+		<label for="mdp">Mot de Passe:</label>
+		<input type="password" name="mdp" id="mdp" />
+		<span class="error"><?php  ?></span>
+		
+		<input type="submit" name="submitted" value="Supprimer mon compte">
 	</form>
-
