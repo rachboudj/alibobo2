98e602f16e2388c1e716e482ae1358170504150c
Akame <Akame@localhost>
2022-07-13 10:38:18 +0200

    sauvegarde pour la pause

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -1,5 +1,16 @@
 <?php
-
+if (isset($_POST['frmInscription'])) {
+	$email = isset($_POST['email']) ? htmlentities(trim($_POST['email'])) : "";
+	$mdp1 = isset($_POST['mdp1']) ? htmlentities(trim($_POST['mdp1'])) :  "";
+	$mdp2 = isset($_POST['mdp2']) ? htmlentities(trim($_POST['mdp2'])) :  "";
+	
+	$errors = [];
+	if (mb_strlen($email) === 0)
+		array_push($errors, "Veuillez rentrer votre l'addresse email de votre compte!!");
+	elseif (!(filter_var($email, FILTER_VALIDATE_EMAIL)))
+		array_push($errors, "");
+	if (mb_strlen($mdp1) === 0 || mb_strlen($mdp2) === 0)
+		array_push($errors, "");
 
 ?>
 	<h1>Login</h1>
