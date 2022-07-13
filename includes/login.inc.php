4f554a8469b10f25df4e3715ae77ff1706be640c
Akame <Akame@localhost>
2022-07-13 11:37:34 +0200

    deuxieme input pour verif mot de passe

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -1,16 +1,4 @@
 <?php
-if (isset($_POST['frmInscription'])) {
-	$email = isset($_POST['email']) ? htmlentities(trim($_POST['email'])) : "";
-	$mdp1 = isset($_POST['mdp1']) ? htmlentities(trim($_POST['mdp1'])) :  "";
-	$mdp2 = isset($_POST['mdp2']) ? htmlentities(trim($_POST['mdp2'])) :  "";
-	
-	$errors = [];
-	if (mb_strlen($email) === 0)
-		array_push($errors, "Veuillez rentrer votre l'addresse email de votre compte!!");
-	elseif (!(filter_var($email, FILTER_VALIDATE_EMAIL)))
-		array_push($errors, "");
-	if (mb_strlen($mdp1) === 0 || mb_strlen($mdp2) === 0)
-		array_push($errors, "");
 
 ?>
 	<h1>Login</h1>
@@ -22,6 +10,10 @@ if (isset($_POST['frmInscription'])) {
 		<label for="mdp">Mot de Passe:</label>
 		<input type="password" name="mdp" id="mdp" />
 		<span class="error"><?php  ?></span>
+		
+		<label for="mdp2">Verification Mot de Passe :</label>
+		<input type="password" name="mdp" id="mdp" />
+		<span class="error"><?php ?></span>
 		
 		<input type="submit" name="submitted" value="Supprimer mon compte">
 	</form>
