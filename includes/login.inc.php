2b1cb80ed9a100481c1b99a515307b7dee8e950c
Akame <Akame@localhost>
2022-07-13 09:55:38 +0200

    ajout d'un id dans le formulaire

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -3,15 +3,7 @@
 
 ?>
 	<h1>Login</h1>
-	<form action="" method="post" class="wrap2" novalidate>
-        	<label for="nom">Nom :</label>
-        	<input type="text" name="nom" id="nom" value="<?php if(!empty($_POST['nom>
-        	<span class="error"><?php if(!empty($errors['nom'])) { echo $errors['nom'>
-
-        	<label for="prenom">Prénom :</label>
-        	<input type="text" name="prenom" id="prenom" value="<?php if(!empty($_POS>
-        	<span class="error"><?php if(!empty($errors['prenom'])) { echo $errors['p>
-
+	<form action="" method="post" class="wrap2" novalidate id="desinscription">
         	<label for="email">E-mail :</label>
         	<input type="email" name="email" id="email" value="<?php if(!empty($_POST>
         	<span class="error"><?php if(!empty($errors['email'])) { echo $errors['em>
