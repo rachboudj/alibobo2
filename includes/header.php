9316d87a7b65d143ac2dddb08740205f35ccbaba
Rod-Ritter <rodolphe.ritter@gmail.com>
2022-07-18 14:55:41 +0200

    ajout de login logout dans le menu de navigation

--- includes/header.php
+++ includes/header.php
@@ -14,7 +14,13 @@
                 <li><a href="index.php?page=accueil">Accueil</a></li>
                 <li><a href="index.php?page=articles">Articles</a></li>
                 <li><a href="index.php?page=inscription">Inscription</a></li>
-                <li><a href="index.php?page=login">Login</a></li>
+                <?php
+                if (isset($_SESSION['login']) && $_SESSION['login'] === true) {
+                    echo '<li><a href="index.php?page=logout">Logout</a></li>';
+                } else {
+                    echo '<li><a href="index.php?page=login">Login</a></li>';                    
+                }
+                ?>
             </ul>
         </nav>
     </header>
