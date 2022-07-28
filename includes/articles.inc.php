e4d0cd83bd9f610f9993a61989ff0635f8714b94
Frédéric NOËL <contact@fredericnoel.com>
2022-07-28 09:56:22 +0200

    Requête pour affichage catégorie premier niveau

--- includes/articles.inc.php
+++ includes/articles.inc.php
@@ -1,1 +1,27 @@
 <h1>Articles</h1>
+<?php
+
+$requeteCategoriesNiveau1 = "
+    SELECT *
+    FROM categories
+    WHERE  categories_id_categorie=0
+    ORDER BY libelle
+";
+
+$connexionCategories = new Sql();
+
+$resultatCaterogies = $connexionCategories->select($requeteCategoriesNiveau1);
+
+$menuCategories = "<ul>";
+
+for ($i = 0 ; $i < count($resultatCaterogies) ; $i++) {
+    $menuCategories .= "<li>";
+    $menuCategories .= "<a href=\"index.php?page=articles&amp;id_categorie=" . $resultatCaterogies[$i]['id_categorie'] . "\">";
+    $menuCategories .= $resultatCaterogies[$i]['libelle'];
+    $menuCategories .= "</a>";
+    $menuCategories .= "</li>";
+}
+
+$menuCategories .= "<ul>";
+
+echo $menuCategories;
